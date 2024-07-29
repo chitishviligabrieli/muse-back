9548036406be20762d286a919d321edcb3b9255f
chitishviligabrieli <157848399+chitishviligabrieli@users.noreply.github.com>
2024-07-29 18:11:28 +0400

    Revert "Merge pull request #11 from chitishviligabrieli/feat/FBLAN-37/Create-User-module"
    This reverts commit 9ef679e037b9755beffa0665e5fd8ff89c35651b, reversing
    changes made to 07d446e6d68b225f0256cbbc051a666150fcb754.

--- src/listeners/dto/update-listener.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateListenerDto } from './create-listener.dto';
-
-export class UpdateListenerDto extends PartialType(CreateListenerDto) {}
