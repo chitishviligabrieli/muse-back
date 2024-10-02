5536c8ecc90ecc8683dd9ff2322160c550da9b9d
chitishviligabrieli <157848399+chitishviligabrieli@users.noreply.github.com>
2024-10-02 18:37:39 +0400

    refactore: search

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -6,17 +6,19 @@ import { ArtistModule } from 'src/artist/artist.module';
 import { AlbumModule } from 'src/album/album.module';
 import { MusicModule } from 'src/music/music.module';
 import { SearchEntity } from './entities/search.entity';
-
+import { MusicRepository } from '../music/music.repository';
+import { ArtistRepository } from '../artist/artist.repository';
+import { AlbumRepository } from '../album/album.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([SearchEntity]),
-    ArtistModule,
+    TypeOrmModule.forFeature([SearchEntity, ArtistRepository, AlbumRepository, MusicRepository]), // Register repositories/entities here
+    ArtistModule,  // Import related modules separately
     AlbumModule,
     MusicModule,
   ],
   providers: [SearchService],
   controllers: [SearchController],
-  exports: [SearchController],
+  exports: [SearchService],  // Export the service if it's needed elsewhere
 })
 export class SearchModule {}
